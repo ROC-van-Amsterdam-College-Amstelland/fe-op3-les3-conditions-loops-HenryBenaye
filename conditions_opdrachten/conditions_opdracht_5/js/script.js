66d35128215bd28624013ce2f2817962abbb5683
Henry Benaye <henry.be@outlook.com>
2019-11-26 12:34:45 +0100

    opdracht 5 conditions klaar

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,17 @@
-//javascript code
+//javascript code`
+
+var myOperator = '+';
+var myNumber1 = 20 ;
+var myNumber2 = 10;
+
+
+if(myOperator == '+'){
+
+    prompt(myNumber1 + myNumber2);
+
+}
+
+if (myOperator == '-' ){
+
+    prompt(myNumber1 - myNumber2);
+}
