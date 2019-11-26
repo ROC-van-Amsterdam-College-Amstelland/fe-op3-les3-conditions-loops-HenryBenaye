0d8fbe2e33d0986e0058428a4f31be2e7122d418
Henry Benaye <henry.be@outlook.com>
2019-11-26 10:50:31 +0100

    Opdract 2 conditions klaar

--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,13 @@
 //javascript code
+
+var banksaldo = 33;
+
+if( banksaldo >= 25) {
+
+    alert("Je hebt voldoende saldo om 25 euro op te nemen.");
+
+}
+
+else{
+    alert("Sorry, je hebt te weinig saldo!")
+}
