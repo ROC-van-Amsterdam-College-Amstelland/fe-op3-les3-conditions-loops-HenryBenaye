5d061317b83f770f7ae602cbd240a830033edac0
Henry Benaye <henry.be@outlook.com>
2019-11-26 10:59:52 +0100

    conditions opdracht 3 klaar

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,15 @@
 //javascript code
+
+var banksaldo = 600;
+
+if(banksaldo == 500){
+
+    alert("Er is salaris gestort");
+
+}
+
+else(banksaldo < 500);{
+
+    alert("Salaris met Bonus!!");
+   
+}
