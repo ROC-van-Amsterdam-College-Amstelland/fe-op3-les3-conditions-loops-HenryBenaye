2fec27c6a5e8a7329871bd2c6744f30a10a1f7e4
Henry Benaye <henry.be@outlook.com>
2019-12-03 13:11:30 +0100

    opdracht 5 klaar

--- loops_opdrachten/loops_opdracht_5/js/script.js
+++ loops_opdrachten/loops_opdracht_5/js/script.js
@@ -1,13 +1,14 @@
 //javascript code
 
 for(teller=1; teller <= 20; teller++){
-    document.getElementById("resultaat").innerHTML += teller;
+    // document.getElementById("resultaat").innerHTML += teller;
     // console.log(teller);
 
 
     // console.log(teller%2);
     if(teller % 2 == 0){
-        console.log(teller+" is een even getal");
+        // console.log(teller+" is een even getal");
+        document.getElementById("resultaat").innerHTML += teller;
     }else{
         console.log(teller+" is een oneven getal");
 
