5f1bba615843f7b723f3f31adc3f64dc3eb98571
Henry Benaye <henry.be@outlook.com>
2019-12-03 13:46:29 +0100

    opdracht 6 bijnaklaar

--- loops_opdrachten/loops_opdracht_6/js/script.js
+++ loops_opdrachten/loops_opdracht_6/js/script.js
@@ -1,1 +1,18 @@
 //javascript code
+
+function cijfer(){
+    var maximum = document.getElementById("invoerCijfer").value; 
+    console.log(maximum);
+
+        
+    for(teller = 1; teller <= maximum; teller++){
+        console.log(maximum);
+        document.getElementById("resultaat").innerHTML += teller;
+
+    }
+}
+
+//maximum getal óphalen'/uitlezen/ value van input bepalen
+
+//maximum variabele gebruiken in for loop
+
