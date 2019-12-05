874fdf6195433a536c0ef1c8dc05b713aeb3ce76
Henry Benaye <henry.be@outlook.com>
2019-12-05 15:06:59 +0100

    opdracht 7 klaar

--- loops_opdrachten/loops_opdracht_7/js/script.js
+++ loops_opdrachten/loops_opdracht_7/js/script.js
@@ -2,7 +2,21 @@
 
 for(teller= 1; teller <= 100; teller++ ){
     console.log(teller);
-    document.getElementById("resultaat").innerHTML += teller + "<br>"; 
-
-    if(teller % 2 == ){}
+    
+    
+    if(teller % 3 == 0){
+        document.getElementById("resultaat").innerHTML += "<strong>VET</strong><br>";
+        console.log(teller);
+    }
+    else{
+        document.getElementById("resultaat").innerHTML +=  teller + "<br>"; 
+    }
+    if(teller % 5 == 0){
+        document.getElementById("resultaat").innerHTML += "<strong>COOL</strong><br>";
+        console.log(teller);
+    }
+    else{
+        document.getElementById("resultaat").innerHTML +=  teller + "<br>"; 
+    }
+    
 }
