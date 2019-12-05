caacc36cee574b1e552bbaadc8f7201d3547f513
Henry Benaye <henry.be@outlook.com>
2019-12-05 15:33:48 +0100

    opdracht 10  klaaar loops zijn done

--- loops_opdrachten/loops_opdracht_10/js/script.js
+++ loops_opdrachten/loops_opdracht_10/js/script.js
@@ -1,1 +1,11 @@
 //javascript code
+
+var abc = ["A", "B", "C", "D", "E"];
+
+// console.log(abc[0] );
+
+for(var teller = 0; teller < 5; teller++){
+    console.log( abc[teller] );
+}
+
+console.log(abc);
