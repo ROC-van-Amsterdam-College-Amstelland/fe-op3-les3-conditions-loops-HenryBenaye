4358b45dbeb9d4e38fde1009b5fde7b35ccea0b9
Henry Benaye <henry.be@outlook.com>
2019-12-06 12:50:09 +0100

    opdracht 6 van conditions is bijna klaar maar ik weet nie hoe verder

--- conditions_opdrachten/conditions_opdracht_6/js/script.js
+++ conditions_opdrachten/conditions_opdracht_6/js/script.js
@@ -1,1 +1,14 @@
 //javascript code
+
+var myOperator = '*';
+
+var myNumber1 = 2;
+var myNumber2 = 5;
+
+if( myOperator == '*' ){
+    prompt(myNumber1 * myNumber2);
+    document.getElementById("resultaat").innerHTML 
+}
+else{
+    prompt(myNumber1 / myNumber2);
+}
